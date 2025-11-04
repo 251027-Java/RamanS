438e143b6f86e769a3f36e633591ca547ff41e69
Raman Shrestha <shrestha.raman1126@gmail.com>
2025-11-04 14:29:53 -0600

    added the guessing game to week 1 folder

---
+++ Week 1/GuessingGame/src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello and Welcome!");
+
+        GameService gameService = new GameService();
+        //gameService.start();
+
+        System.out.println("Thanks for playing!");
+    }
+}
